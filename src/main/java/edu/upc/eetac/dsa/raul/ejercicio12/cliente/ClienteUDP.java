7dfbb016e731807d65b94fa8072fa8a6ac5175b3
Raul Suarez <raul.suarez.marin@gmail.com>
2013-10-12 16:08:04 +0200

    Eliminacion de 2 packages

--- src/main/java/edu/upc/eetac/dsa/raul/ejercicio12/cliente/ClienteUDP.java
+++ src/main/java/edu/upc/eetac/dsa/raul/ejercicio12/cliente/ClienteUDP.java
@@ -1,7 +1,5 @@
 package edu.upc.eetac.dsa.raul.ejercicio12.cliente;
 
-import edu.upc.eetac.dsa.raul.red.cliente.QueHoraEsProtocolCliente;
-
 public class ClienteUDP {
 	public static void main(String[] args) {
 		System.out.println("Cliente iniciado");
